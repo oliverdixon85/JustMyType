474271b26bd8ba1f7cd4ac9355088e97fb1ac51b
oliverdixon85 <dixon.oliver@gmail.com>
2019-09-08 11:50:48 +0300

    first try

---
+++ app.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+
+    let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+    let sentenceIndex = 0;
+    let currentSentence = sentences[sentenceIndex];
+    let letterIndex = 0;
+    let currentLetter = currentSentence[letterIndex];
+
+    $('#sentence').text(currentSentence);
+    $('#target-letter').text(currentLetter);
+
+$('#keyboard-upper-container').hide()
+
+$('body').keydown(function(e){
+    if (e.which === 16) {
+        $('#keyboard-upper-container').show()
+        $('#keyboard-lower-container').hide()
+    }
+})
+
+$('body').keyup(function(e) {
+    if (e.which == 16) {
+        $('#keyboard-upper-container').hide()
+        $('#keyboard-lower-container').show()
+    }
+    $('.make-it-yellow').removeClass('make-it-yellow');
+   
+});
+
+$('body').keypress(function(e){
+    $('#' + e.which).addClass('make-it-yellow');
+    
+
+})
+
+// $(document).keypress(function (e) {
+//     if ((e.which < 65) && (e.which > 26)) {
+//         $('#' + e.which).css('background-color', 'yellow');
+//     }
+// });
+
+
+
+})
